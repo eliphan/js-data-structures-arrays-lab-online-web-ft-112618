a4082f422013fdcdf1724cf2dd3f782920de2bdd
Elise Phan <aliceconnic@gmail.com>
2019-03-13 23:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ function prependDriver (name) {
   return [name, ...drivers];
 }
 
-
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
 
 function removeFirstDriver () {
   return drivers.slice(1);
